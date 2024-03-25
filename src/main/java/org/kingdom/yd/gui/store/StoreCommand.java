c5e28549f22261da76a49d1b5080af0698cbb751
9September <ydking0911@gmail.com>
2024-03-25 17:46:08 +0900

    update test

---
+++ src/main/java/org/kingdom/yd/gui/store/StoreCommand.java
@@ -0,0 +1,16 @@
+package org.kingdom.yd.gui.store;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class StoreCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+
+
+
+        return false;
+    }
+}
